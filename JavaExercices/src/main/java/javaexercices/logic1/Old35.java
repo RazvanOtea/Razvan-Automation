4bf4326ef9e45d18dca94340a4d01a9404219d06
Razvan Otea <{94829540}+{RazvanOtea}@users.noreply.github.com>
2023-08-03 12:28:34 +0300

    Completed exercices for Java Logic 1

---
+++ JavaExercices/src/main/java/javaexercices/logic1/Old35.java
@@ -0,0 +1,17 @@
+package javaexercices.logic1;
+
+/*
+Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator -- see Introduction to Mod
+
+
+old35(3) → true
+old35(10) → true
+old35(15) → false
+ */
+public class Old35 {
+    public boolean old35(int n) {
+        boolean isMultipleOf3 = (n % 3 == 0);
+        boolean isMultipleOf5 = (n % 5 == 0);
+        return (isMultipleOf3 || isMultipleOf5) && !(isMultipleOf3 && isMultipleOf5);
+    }
+}
